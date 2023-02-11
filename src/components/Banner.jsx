d2d3e7ab8f6e920063cd414627ba3ffe204c5ba9
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-02-11 20:31:28 +0100

    Minor responsive issues fixed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,8 +4,8 @@ import background from "../assets/images/bg-pattern-circles.svg";
 function Banner() {
   return (
     <>
-      <img className="banner-bg-phones-mobile" src={image} alt="#" />
       <section className="banner">
+        <img className="banner-bg-phones-mobile" src={image} alt="#" />
         <div className="banner-wrapper banner-content">
           <h3>State of the Art Infrastructure</h3>
           <p>With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. This ensures your site will load instantly, no matter where your readers are, keeping your site competetive.</p>
